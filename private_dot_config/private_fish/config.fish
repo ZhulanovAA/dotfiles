4859147841970ecda47e25f5ba226480adce8a31
ZhulanovAA <devZhulanov.A.A@gmail.com>
2025-05-14 16:07:20 +0500

    Add starship config

--- private_dot_config/private_fish/config.fish
+++ private_dot_config/private_fish/config.fish
@@ -1,5 +1,6 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
     zoxide init fish | source
+    set -gx STARSHIP_CONFIG ~/.config/starship/starship.toml
     starship init fish | source
 end
